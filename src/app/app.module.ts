a072769ed6880f7877383e580851a73b3295511b
AJMikalauskas <97716972+AJMikalauskas@users.noreply.github.com>
2022-06-06 10:44:51 -0500

    Added pwa package when changed version to less than v14, now having problem with url for the offline server of localhost:8080, the apiUrl is using the environment prod ts url which I didn;' change until I ran the server, but now when I change it, results in apiKey being invalid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
-//import {ServiceWorkerModule} from "@angular/service-worker";
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WeatherComponent } from './weather/weather.component';
 
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 import {MatCardModule} from "@angular/material/card";
 import {MatInputModule} from "@angular/material/input";
 import {MatIconModule} from "@angular/material/icon";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -23,7 +25,14 @@ import {MatIconModule} from "@angular/material/icon";
     HttpClientModule,
     MatIconModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatSnackBarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     // API Url and API key
     apiUrl: "https://api.openweathermap.org/data/2.5/",
-    apiKey: "<326bebe1d2367985a3ce843d8c71659a>",
-  production: true
+    apiKey: "326bebe1d2367985a3ce843d8c71659a",
+    production: true
 };
